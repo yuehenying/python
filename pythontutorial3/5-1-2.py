c1f30575312d85973ca6ed4d88806895b9e4a5d3
yuehenying <761546631@qq.com>
2019-08-06 16:03:58 +0800

    Delete 5-1-2.py

--- pythontutorial3/5-1-2.py
+++
@@ -1,8 +0,0 @@
-from collections import deque
-#把列表当作队列使用，先进先出
-queue = deque(["Eric","John","Michael"])
-queue.append("Terry")
-queue.append("Graham")
-queue.popleft()
-queue.popleft()
-print(queue)
